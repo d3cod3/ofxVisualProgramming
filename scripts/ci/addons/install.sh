e2aa13ea741bc501fe7f657fc46bf4017d5267c7
Emanuele Mazza <n3m3da@d3cod3.org>
2018-06-28 13:32:10 +0200

    linux fftw3 compile on build, trying to fix travis compile error (3)

--- scripts/ci/addons/install.sh
+++ scripts/ci/addons/install.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+
+if [ "$TRAVIS_OS_NAME" = "linux" -o -z "$TRAVIS_OS_NAME" ]; then
+  curl -SLsO https://raw.githubusercontent.com/d3cod3/ofxVisualProgramming/fftw/fftw-3.3.2.tar.gz
+  tar -xvf fftw-3.3.2.tar.gz
+  cd fftw-3.3.2
+  ./configure --prefix=`pwd` --enable-float --enable-sse2 --with-incoming-stack-boundary=2 --with-our-malloc16 --disable-shared --enable-static
+  make
+  cd .libs
+  cp libfftw3f.a ~/openFrameworks/addons/ofxAudioAnalyzer/libs/fftw3f/lib/linux64/
+fi
