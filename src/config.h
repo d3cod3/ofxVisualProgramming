0079524239977ee9d5bd5ca35353970fefbc57e6
Emanuele Mazza <n3m3da@d3cod3.org>
2019-05-16 08:56:34 +0200

    more saturated cables colors

--- src/config.h
+++ src/config.h
@@ -63,16 +63,16 @@
 #define MAX_INLETS              24
 #define MAX_OUTLETS             24
 
-#define COLOR_NUMERIC_LINK      ofColor(250,250,250,255)
-#define COLOR_STRING_LINK       ofColor(230,210,255,255)
-#define COLOR_ARRAY_LINK        ofColor(255,255,200,255)
-#define COLOR_TEXTURE_LINK      ofColor(200,255,255,255)
-#define COLOR_AUDIO_LINK        ofColor(255,255,200,255)
-#define COLOR_SCRIPT_LINK       ofColor(255,180,180,255)
+#define COLOR_NUMERIC_LINK      ofColor(210,210,210,255)
+#define COLOR_STRING_LINK       ofColor(200,180,255,255)
+#define COLOR_ARRAY_LINK        ofColor(120,255,120,255)
+#define COLOR_TEXTURE_LINK      ofColor(120,255,255,255)
+#define COLOR_AUDIO_LINK        ofColor(255,255,120,255)
+#define COLOR_SCRIPT_LINK       ofColor(255,128,128,255)
 
 #define COLOR_NUMERIC           ofColor(210,210,210,255)
 #define COLOR_STRING            ofColor(200,180,255,255)
-#define COLOR_ARRAY             ofColor(200,255,200,255)
+#define COLOR_ARRAY             ofColor(120,255,120,255)
 #define COLOR_TEXTURE           ofColor(120,255,255,255)
 #define COLOR_AUDIO             ofColor(255,255,120,255)
 #define COLOR_SCRIPT            ofColor(255,128,128,255)
