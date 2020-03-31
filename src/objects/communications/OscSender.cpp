38531ea6b7a9de06528e8c0785a5ae1cba0a15e8
d3cod3 <n3m3da@d3cod3.org>
2020-03-31 12:08:21 +0200

    limited OSC blob send to 1280x720 grayscale image

--- src/objects/communications/OscSender.cpp
+++ src/objects/communications/OscSender.cpp
@@ -156,7 +156,7 @@ void OscSender::updateObjectContent(map<int,shared_ptr<PatchObject>> &patchObjec
                 }else if(static_cast<ofTexture *>(_inletParams[i])->getTextureData().glInternalFormat == GL_RGBA ||static_cast<ofTexture *>(_inletParams[i])->getTextureData().glInternalFormat == GL_RGBA16 || static_cast<ofTexture *>(_inletParams[i])->getTextureData().glInternalFormat == GL_RGBA32F_ARB){
                     depth = 4;
                 }
-                if(static_cast<ofTexture *>(_inletParams[i])->getWidth()*static_cast<ofTexture *>(_inletParams[i])->getHeight()*depth < 327680){
+                if(static_cast<ofTexture *>(_inletParams[i])->getWidth()*static_cast<ofTexture *>(_inletParams[i])->getHeight()*depth < 922000){ // 327680
                     static_cast<ofTexture *>(_inletParams[i])->readToPixels(*_tempPixels);
                     m.addFloatArg(static_cast<ofTexture *>(_inletParams[i])->getWidth());
                     m.addFloatArg(static_cast<ofTexture *>(_inletParams[i])->getHeight());
