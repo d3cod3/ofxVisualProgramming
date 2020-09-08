94893cd4de92c6c31c34d7828c81f72aea538fa4
d3cod3 <n3m3da@d3cod3.org>
2020-09-08 10:40:29 +0200

    added color palette generator object

---
+++ src/objects/graphics/ColorPalette.h
@@ -0,0 +1,95 @@
+/*==============================================================================
+
+    ofxVisualProgramming: A visual programming patching environment for OF
+
+    Copyright (c) 2019 Emanuele Mazza aka n3m3da <emanuelemazza@d3cod3.org>
+
+    ofxVisualProgramming is distributed under the MIT License.
+    This gives everyone the freedoms to use ofxVisualProgramming in any context:
+    commercial or non-commercial, public or private, open or closed source.
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+    OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+
+    See https://github.com/d3cod3/ofxVisualProgramming for documentation
+
+==============================================================================*/
+
+#ifndef OFXVP_BUILD_WITH_MINIMAL_OBJECTS
+
+#pragma once
+
+#include "PatchObject.h"
+
+enum ColorChannel{
+    SATURATION,
+    BRIGHTNESS
+};
+
+class ColorPalette : public PatchObject {
+
+public:
+
+    ColorPalette();
+
+    void            newObject() override;
+    void            setupObjectContent(shared_ptr<ofAppGLFWWindow> &mainWindow) override;
+    void            updateObjectContent(map<int,shared_ptr<PatchObject>> &patchObjects) override;
+
+    void            drawObjectContent(ofxFontStash *font, shared_ptr<ofBaseGLRenderer>& glRenderer) override;
+    void            drawObjectNodeGui( ImGuiEx::NodeCanvas& _nodeCanvas ) override;
+
+    void            removeObjectContent(bool removeFileFromData=false) override;
+
+    void            initPalette();
+    void            generateRandom(int numColors);
+    void            generateMonoChromatic(ColorChannel channel = SATURATION, int numColors=4);
+    void            generateComplementary(ColorChannel channel = SATURATION, int numColors=4);
+    void            generateTriad();
+    void            generateComplementaryTriad();
+    void            generateAnalogous(int numColors, float spread);
+    float           normalizeValue(float val);
+
+
+    ofFloatColor            baseColor;
+    vector<ofFloatColor>    palette;
+    vector<string>          generationOptions;
+    vector<string>          channelOptions;
+    int                     selectedGeneration;
+    int                     selectedChannel;
+    int                     numColors;
+    float                   spread;
+
+    float               posX, posY, drawW, drawH;
+
+    float               scaledObjW, scaledObjH;
+    float               objOriginX, objOriginY;
+    float               canvasZoom;
+
+    bool                loaded;
+
+protected:
+
+
+private:
+
+    OBJECT_FACTORY_PROPS
+
+};
+
+#endif
