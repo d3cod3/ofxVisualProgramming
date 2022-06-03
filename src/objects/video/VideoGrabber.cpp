daa7a653187e811cde3dad08d8112f533747ced6
d3cod3 <n3m3da@d3cod3.org>
2022-06-03 18:27:24 +0200

    fixed type comparison warning

--- src/objects/video/VideoGrabber.cpp
+++ src/objects/video/VideoGrabber.cpp
@@ -232,7 +232,7 @@ void VideoGrabber::drawObjectNodeConfig(){
     ImGui::Text("Format: %ix%i",camWidth,camHeight);
 
     ImGui::Spacing();
-    if(ImGui::BeginCombo("Device", devicesVector.size()>deviceID ? devicesVector.at(deviceID).c_str() : deviceName.c_str() )){
+    if(ImGui::BeginCombo("Device", static_cast<int>(devicesVector.size())>deviceID ? devicesVector.at(deviceID).c_str() : deviceName.c_str() )){
         for(int i=0; i < static_cast<int>(devicesVector.size()); ++i){
             bool is_selected = (deviceID == i );
             if (ImGui::Selectable(devicesVector.at(i).c_str(), is_selected)){
