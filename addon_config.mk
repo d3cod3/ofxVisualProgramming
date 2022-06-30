70d4dd53503f9800c61bbfa1c00e8ac25e268881
d3cod3 <n3m3da@d3cod3.org>
2022-06-30 18:37:23 +0200

    removed ofxPython and python script object

--- addon_config.mk
+++ addon_config.mk
@@ -74,11 +74,11 @@ common:
 
 
 linux64:
-        ADDON_DEPENDENCIES += ofxHapPlayer ofxPd ofxPdExternals ofxPython ofxNDI
+        ADDON_DEPENDENCIES += ofxHapPlayer ofxPd ofxPdExternals ofxNDI
         ADDON_SOURCES_EXCLUDE = src/objects/video/SyphonSender% src/objects/video/SyphonReceiver%
 
 msys2:
-        ADDON_SOURCES_EXCLUDE = src/objects/scripting/BashScript% src/objects/scripting/PythonScript% src/objects/sound/PDPatch% src/objects/video/VideoSender% src/objects/video/VideoReceiver% src/objects/video/SyphonSender% src/objects/video/SyphonReceiver%
+        ADDON_SOURCES_EXCLUDE = src/objects/scripting/BashScript% src/objects/sound/PDPatch% src/objects/video/VideoSender% src/objects/video/VideoReceiver% src/objects/video/SyphonSender% src/objects/video/SyphonReceiver%
 
 osx:
-        ADDON_DEPENDENCIES += ofxHapPlayer ofxPd ofxPdExternals ofxPython ofxNDI ofxSyphon
+        ADDON_DEPENDENCIES += ofxHapPlayer ofxPd ofxPdExternals ofxNDI ofxSyphon
