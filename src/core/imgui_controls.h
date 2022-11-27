c70754974bf2fa4c0e774bccc6517d9800998cdf
d3cod3 <n3m3da@d3cod3.org>
2022-11-27 18:28:06 +0100

    added Pad2D rendering for objects menu

--- src/core/imgui_controls.h
+++ src/core/imgui_controls.h
@@ -23,6 +23,7 @@ namespace ImGuiEx {
 bool EnvelopeEditor(ImDrawList* drawList, float width, float height, float *_a, float *_h, float *_s, float *_r, int type=ImGuiEnvelopeEditorType_ADSR);
 
 bool Pad2D(ImDrawList* drawList, float width, float height,float *_x, float *_y);
+bool Pad2D(float width, float height,float *_x, float *_y);
 
 // Minimal implementation modified from: https://github.com/ocornut/imgui/issues/942
 bool KnobFloat(ImDrawList* draw_list, float width, ImU32 color, const char* label, float* p_value, float v_min, float v_max, float v_step=50.f);
