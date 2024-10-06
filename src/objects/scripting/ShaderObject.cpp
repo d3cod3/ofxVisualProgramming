ca15d2fd53782a3de495423835eea070fc7f3101
d3cod3 <n3m3da@d3cod3.org>
2024-10-06 11:18:34 +0200

    removed old python object reference

--- src/objects/scripting/ShaderObject.cpp
+++ src/objects/scripting/ShaderObject.cpp
@@ -133,7 +133,7 @@ void ShaderObject::updateObjectContent(map<int,shared_ptr<PatchObject>> &patchOb
             if(patchObjects[it->first] != nullptr && it->first != this->getId() && !patchObjects[it->first]->getWillErase()){
                 for(int o=0;o<static_cast<int>(it->second->outPut.size());o++){
                     if(!it->second->outPut[o]->isDisabled && it->second->outPut[o]->toObjectID == this->getId()){
-                        if(it->second->getName() == "lua script" || it->second->getName() == "python script" || it->second->getName() == "glsl shader"){
+                        if(it->second->getName() == "lua script" || it->second->getName() == "glsl shader"){
                             it->second->resetResolution(this->getId(),output_width,output_height);
                         }
                     }
