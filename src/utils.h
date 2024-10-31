a0df2a9e2db897658bf86e6002122747dc86b629
d3cod3 <n3m3da@d3cod3.org>
2024-10-31 21:30:48 +0100

    added string util function

--- src/utils.h
+++ src/utils.h
@@ -101,6 +101,22 @@ inline std::string& sanitizeFilename(std::string& s){
 }
 
 //--------------------------------------------------------------
+inline std::vector<std::string> splitStringWithDelimiter(std::string s, std::string delimiter) {
+    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+    std::string token;
+    std::vector<std::string> res;
+
+    while ((pos_end = s.find(delimiter, pos_start)) != std::string::npos) {
+        token = s.substr (pos_start, pos_end - pos_start);
+        pos_start = pos_end + delim_len;
+        res.push_back (token);
+    }
+
+    res.push_back (s.substr (pos_start));
+    return res;
+}
+
+//--------------------------------------------------------------
 inline int nthOccurrence(const std::string& str, const std::string& findMe, int nth){
     size_t  pos = 0;
     int     cnt = 0;
