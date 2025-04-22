f2ff0fa0a1d8b95ade4eeb689966de80a3cd4438
d3cod3 <n3m3da@d3cod3.org>
2025-04-22 18:35:43 +0100

    standard texture resolution set at HD 1920x1080 ( it was time )

--- src/ofxVPConfig.h
+++ src/ofxVPConfig.h
@@ -55,8 +55,8 @@
 #define STANDARD_PROJECTOR_WINDOW_WIDTH     854
 #define STANDARD_PROJECTOR_WINDOW_HEIGHT    480
 
-#define STANDARD_TEXTURE_WIDTH              1280
-#define STANDARD_TEXTURE_HEIGHT             720
+#define STANDARD_TEXTURE_WIDTH              1920
+#define STANDARD_TEXTURE_HEIGHT             1080
 
 #define OBJECT_STANDARD_WIDTH   160
 #define OBJECT_STANDARD_HEIGHT  120
@@ -107,6 +107,7 @@
 #define OFXVP_OBJECT_CAT_COMMUNICATIONS "communications"
 #define OFXVP_OBJECT_CAT_CV             "computer vision"
 #define OFXVP_OBJECT_CAT_DATA           "data"
+#define OFXVP_OBJECT_CAT_GENERATIVE     "generative"
 #define OFXVP_OBJECT_CAT_GRAPHICS       "graphics"
 #define OFXVP_OBJECT_CAT_GUI            "gui"
 #define OFXVP_OBJECT_CAT_LOGIC          "logic"
